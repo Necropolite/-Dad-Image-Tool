7344ef41d013b79e5c5f95cebde39dcdf2d1cf48
Clinton Ramey <cramey254@gmail.com>
2026-08-06 06:51:48 -0400

    Update uninstaller for watched-folder design

--- Uninstall.bat
+++ Uninstall.bat
@@ -3,11 +3,17 @@ setlocal
 title Dad Image Tool Uninstaller
 
 echo Removing Dad Image Tool...
-reg delete "HKCU\Software\Classes\dadimage" /f >nul 2>nul
+
+taskkill /im "Dad Image Tool.exe" /f >nul 2>nul
+
 if exist "%UserProfile%\Desktop\Dad Image Tool.lnk" del /q "%UserProfile%\Desktop\Dad Image Tool.lnk"
+if exist "%UserProfile%\Desktop\Drop Client Pictures Here.lnk" del /q "%UserProfile%\Desktop\Drop Client Pictures Here.lnk"
+if exist "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\Dad Image Tool.lnk" del /q "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup\Dad Image Tool.lnk"
 if exist "%LocalAppData%\Dad Image Tool" rmdir /s /q "%LocalAppData%\Dad Image Tool"
 
 echo.
 echo Dad Image Tool was removed.
-echo The browser extension can be removed from the browser's Extensions page.
+echo.
+echo Client pictures under Pictures\Dad Image Tool were not deleted.
+echo Delete that folder manually only after confirming it is no longer needed.
 pause
